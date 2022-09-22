cc43d70de4517d77df035b4fb269a761cd467e8f
Matvey Batishchev <batishevmatvey@mail.ru>
2022-09-22 23:53:14 +0300

    Initial commit IPA-11

---
+++ src/main/java/ru/investing_portal/InvestingPortalApplication.java
@@ -0,0 +1,13 @@
+package ru.investing_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestingPortalApplication.class, args);
+    }
+
+}
