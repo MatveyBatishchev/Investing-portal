8fa684ddd686f2c96c3298a435856bb089940eb6
Matvey Batishchev <batishevmatvey@mail.ru>
2022-10-20 13:09:28 +0300

    Связал id c бд-sequence, добавил модель Watchlist IPA-20

---
+++ src/main/java/ru/investing_portal/models/domain/Watchlist.java
@@ -0,0 +1,43 @@
+package ru.investing_portal.models.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name="watchlist")
+public class Watchlist {
+
+    /**
+     * Идентификатор watchlist-а
+     */
+    @Id
+    @SequenceGenerator(name="watchlist_sequence", sequenceName = "watchlist_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="watchlist_sequence")
+    @Column(name="id")
+    private int id;
+
+    /**
+     * Название портфолио
+     */
+    @Column(name="name")
+    private String name;
+
+    /**
+     * Описание watchlist-а
+     */
+    @Column(name="comments")
+    private String comments;
+
+    /**
+     * Список монеты связанных с watchlist-ом
+     */
+    @ManyToMany
+    @JoinTable(name="watchlist_coin",
+            joinColumns={@JoinColumn(name="watchlist_id")},
+            inverseJoinColumns={@JoinColumn(name="coin_id")})
+    private Set<Coin> coin;
+
+}
